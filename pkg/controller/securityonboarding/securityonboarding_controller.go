f6467772040fd4ac60716085423bc110910fd925
Rashmi Khanna <rashmi.khanna@in.ibm.com>
2020-03-26 03:15:33 -0700

    add library

--- pkg/controller/securityonboarding/securityonboarding_controller.go
+++ pkg/controller/securityonboarding/securityonboarding_controller.go
@@ -29,6 +29,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+        "reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"

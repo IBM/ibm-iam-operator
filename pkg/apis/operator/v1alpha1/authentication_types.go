fd37ff61a9356810df18593c3dae264e19a0d2cc
Rashmi Khanna <k.rashmi.r@gmail.com>
2020-08-20 15:18:05 +0530

    Update authentication_types.go

--- pkg/apis/operator/v1alpha1/authentication_types.go
+++ pkg/apis/operator/v1alpha1/authentication_types.go
@@ -108,6 +108,9 @@ type ConfigSpec struct {
 	ROKSURL                     string `json:"roksURL"`
 	ROKSUserPrefix              string `json:"roksUserPrefix"`
 	EnableImpersonation         bool   `json:"enableImpersonation"`
+	CLAIMS_SUPPORTED            string `json:"claimsSupported"`
+	CLAIMS_MAP                  string `json:"claimsMap"`
+	SCOPE_CLAIM                 string `json:"scopeClaim"`
 	OIDCIssuerURL               string `json:"oidcIssuerURL"`
 }
 

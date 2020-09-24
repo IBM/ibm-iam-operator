f7476b4aa3813c92da22eccd9a2165142cd75b63
Rashmi Khanna <k.rashmi.r@gmail.com>
2020-09-24 22:45:11 +0530

    Update ingress.go

--- pkg/controller/authentication/ingress.go
+++ pkg/controller/authentication/ingress.go
@@ -761,7 +761,7 @@ func platformOidcIngress(instance *operatorv1alpha1.Authentication, scheme *runt
 	reqLogger := log.WithValues("Instance.Namespace", instance.Namespace, "Instance.Name", instance.Name)
 	var xframeDomain string
 	if (instance.Spec.Config.XFrameDomain != "") {
-		xframeDomain = strings.Join([]string{"'ALLOW-FROM", instance.Spec.Config.XFrameDomain}, " ")
+		xframeDomain = strings.Join([]string{"'ALLOW-FROM ", instance.Spec.Config.XFrameDomain, "'"}, "")		
 	} else {
 		xframeDomain = "'SAMEORIGIN'"
 	}	

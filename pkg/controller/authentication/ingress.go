d6f914d221a63b04a8e1994d925fe2e7d66d205b
Magesh Kumar Sivaswamy <smagesh2106@gmail.com>
2021-05-14 18:24:16 +0530

    Update ingress.go

--- pkg/controller/authentication/ingress.go
+++ pkg/controller/authentication/ingress.go
@@ -618,7 +618,7 @@ func platformIdProviderIngress(instance *operatorv1alpha1.Authentication, scheme
 				"icp.management.ibm.com/secure-backends": "true",
 				"icp.management.ibm.com/rewrite-target":  "/",
 				"icp.management.ibm.com/authz-type":      "rbac",
-				"icp.management.ibm.com/configuration-snippet": "\n            limit_req zone=management-ingress-rps-1000 burst=100 nodelay;",
+				"icp.management.ibm.com/configuration-snippet": "\n            limit_req zone=management-ingress-rps-100 burst=20 nodelay;",
 			},
 		},
 		Spec: net.IngressSpec{

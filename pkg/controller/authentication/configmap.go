9bb8307e9a9d55889e01c315262d68665684eacd
Rashmi Khanna <k.rashmi.r@gmail.com>
2021-06-29 00:17:53 +0530

    Update configmap.go

--- pkg/controller/authentication/configmap.go
+++ pkg/controller/authentication/configmap.go
@@ -368,7 +368,7 @@ func registrationJsonConfigMap(instance *operatorv1alpha1.Authentication, wlpCli
 
 func registrationScriptConfigMap(instance *operatorv1alpha1.Authentication, scheme *runtime.Scheme) *corev1.ConfigMap {
 
-	//reqLogger := log.WithValues("Instance.Namespace", instance.Namespace, "Instance.Name", instance.Name)
+	reqLogger := log.WithValues("Instance.Namespace", instance.Namespace, "Instance.Name", instance.Name)
 	newConfigMap := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "registration-script",
@@ -392,7 +392,7 @@ func registrationScriptConfigMap(instance *operatorv1alpha1.Authentication, sche
 
 func oauthClientConfigMap(instance *operatorv1alpha1.Authentication, icpConsoleURL string, icpProxyURL string, scheme *runtime.Scheme) *corev1.ConfigMap {
 
-	//reqLogger := log.WithValues("Instance.Namespace", instance.Namespace, "Instance.Name", instance.Name)
+	reqLogger := log.WithValues("Instance.Namespace", instance.Namespace, "Instance.Name", instance.Name)
 	newConfigMap := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "oauth-client-map",

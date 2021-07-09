0b74512fc64a9a62267e04e50bb948afcfb17caa
Rashmi Khanna <rashmi.khanna@in.ibm.com>
2021-07-09 20:21:06 +0530

    change side effect

--- pkg/controller/authentication/webhook.go
+++ pkg/controller/authentication/webhook.go
@@ -119,7 +119,7 @@ func generateWebhookObject(instance *operatorv1alpha1.Authentication, scheme *ru
 		// multiple deployment in on-prem mode
 		hooksName = instance.Namespace + "." + hooksName
 	}
-	sideEffectClass := reg.SideEffectClass("Unknown")
+	sideEffectClass := reg.SideEffectClass("None")
 	newWebhook := &reg.MutatingWebhookConfiguration{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: webhook,

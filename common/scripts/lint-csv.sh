492a841bab091eca44016d8a9594fcd3aebc3274
Tirumala Mannaru <tmannaru@in.ibm.com>
2022-12-06 19:52:59 +0530

    version bump for CS 3.23.0

--- common/scripts/lint-csv.sh
+++ common/scripts/lint-csv.sh
@@ -18,7 +18,7 @@
 STATUS=0
 ARCH=$(uname -m)
 OS=$(uname -s)
-VERSION=${CSV_VERSION:-3.22.0}
+VERSION=${CSV_VERSION:-3.23.0}
 
 if [[ $ARCH == "x86_64" && $OS == "Linux" ]]; then
     curl -L -o /tmp/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64

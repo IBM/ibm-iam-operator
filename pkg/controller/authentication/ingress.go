8913397ca5032da2937e5922c87d3ac652614019
rashmi_kh <rashmi.khanna@in.ibm.com>
2023-01-23 17:07:59 +0530

    remove ing

--- pkg/controller/authentication/ingress.go
+++ pkg/controller/authentication/ingress.go
@@ -35,7 +35,7 @@ func (r *ReconcileAuthentication) handleIngress(instance *operatorv1alpha1.Authe
 		"platform-auth", "platform-id-auth-block", "platform-id-auth", "platform-id-provider", "platform-login", "platform-oidc-block", "platform-oidc",
 		"saml-ui-callback", "version-idmgmt", "social-login-callback"}
 
-	functionList := []func(*operatorv1alpha1.Authentication, *runtime.Scheme) *net.Ingress{apiKeyIngress, iamTokenRedirectIngress, iamTokenIngress, ibmidUiCallbackIngress, idMgmtIngress, idmgmtV2ApiIngress,
+	functionList := []func(*operatorv1alpha1.Authentication, *runtime.Scheme) *net.Ingress{apiKeyIngress, iamTokenIngress, ibmidUiCallbackIngress, idMgmtIngress, idmgmtV2ApiIngress,
 		platformAuthIngress, platformIdAuthBlockIngress, platformIdAuthIngress, platformIdProviderIngress, platformLoginIngress, platformOidcBlockIngress, platformOidcIngress, platformOidcIntrospectIngress,
 		tokenServiceVersionIngress, samlUiCallbackIngress, versionIdmgmtIngress, socialLoginCallbackIngress}
 

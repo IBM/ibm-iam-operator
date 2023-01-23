958bb9dc9a688f76b3008d428f4735f757c7d785
“Biju <Biju.duari@ibm.com>
2023-01-23 22:14:17 +0530

    Update artifactory

--- common/scripts/config_docker.sh
+++ common/scripts/config_docker.sh
@@ -16,8 +16,7 @@
 #
 
 KUBECTL=$(command -v kubectl)
-DOCKER_REGISTRY="hyc-cloud-private-integration-docker-local.artifactory.swg-devops.com"
-DOCKER_EDGE_REGISTRY="hyc-cloud-private-edge-docker-local.artifactory.swg-devops.com"
+DOCKER_REGISTRY="docker-na-public.artifactory.swg-devops.com"
 
 DOCKER_USERNAME=$(${KUBECTL} -n default get secret artifactory-cred -o jsonpath='{.data.username}' | base64 --decode)
 DOCKER_PASSWORD=$(${KUBECTL} -n default get secret artifactory-cred -o jsonpath='{.data.password}' | base64 --decode)

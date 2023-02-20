f1f75da264b93a136d125612bbc6940ceba2911b
rashmi_kh <rashmi.khanna@in.ibm.com>
2023-02-20 14:54:14 +0530

    add saml-cert
    Signed-off-by: rashmi_kh <rashmi.khanna@in.ibm.com>

--- pkg/controller/authentication/containers.go
+++ pkg/controller/authentication/containers.go
@@ -336,10 +336,10 @@ func buildAuthServiceContainer(instance *operatorv1alpha1.Authentication, authSe
 				Name:      "mongodb-client-cert",
 				MountPath: "/certs/mongodb-client",
 			},
-		},
-		{
-			Name:      "saml-cert",
-			MountPath: "/certs/router-certs",
+			{
+				Name:      "saml-cert",
+				MountPath: "/certs/router-certs",
+			},
 		},
 		ReadinessProbe: &corev1.Probe{
 			ProbeHandler: corev1.ProbeHandler{

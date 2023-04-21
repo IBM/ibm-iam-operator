553d612afdf42529e253de447db81a91e4c30e72
Tirumala Mannaru <tmannaru@in.ibm.com>
2023-04-21 15:47:45 +0530

    zenAuditUrl inclusion to zenInstance collection (#622)

--- pkg/controller/client/clientreg.go
+++ pkg/controller/client/clientreg.go
@@ -69,6 +69,7 @@ type ZenInstance struct {
 	InstanceId     string `json:"instanceId"`
 	ProductNameUrl string `json:"productNameUrl"`
 	Namespace      string `json:"namespace"`
+	ZenAuditUrl    string `json:"zenAuditUrl"`
 }
 
 const (
@@ -429,6 +430,7 @@ func (r *ReconcileClient) CreateZenInstance(ctx context.Context, client *oidcv1.
 		"instanceId":     client.Spec.ZenInstanceId,
 		"productNameUrl": client.Spec.ZenProductNameUrl,
 		"namespace":      client.Namespace,
+		"zenAuditUrl":    client.Spec.ZenAuditUrl,
 	}
 	payloadBytes, _ := json.Marshal(payloadJSON)
 	payload := string(payloadBytes[:])

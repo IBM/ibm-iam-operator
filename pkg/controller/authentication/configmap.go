48bee1c64ccd85e950c7a874015004a191c47b5d
Ujjwal Chakraborty <53339037+ujjwalchk-it@users.noreply.github.com>
2023-05-15 17:25:06 +0530

    domain name fix

--- pkg/controller/authentication/configmap.go
+++ pkg/controller/authentication/configmap.go
@@ -73,7 +73,7 @@ func (r *ReconcileAuthentication) handleConfigMap(instance *operatorv1alpha1.Aut
 	if r.RunningOnCNCFCluster() {
 		isOSEnv = false
 		reqLogger.Info("Checked cluster type", "Configmap.Namespace", instance.Namespace, "ConfigMap.Name", globalConfigMapName, "clusterType", r.clusterType)
-		domainName := globalConfigMap.Data["domain_name"]
+		domainName = globalConfigMap.Data["domain_name"]
 		reqLogger.Info("Reading domainName from global cm", "Configmap.Namespace", instance.Namespace, "ConfigMap.Name", globalConfigMapName, "domainName", domainName)
 	}
 
@@ -81,7 +81,7 @@ func (r *ReconcileAuthentication) handleConfigMap(instance *operatorv1alpha1.Aut
 	err = r.client.Get(context.TODO(), types.NamespacedName{Name: "ibmcloud-cluster-info", Namespace: instance.Namespace}, currentConfigMap)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			reqLogger.Info("Configmap is not found ", "Configmap.Namespace", currentConfigMap.Namespace, "ConfigMap.Name", "ibmcloud-cluster-info")
+			reqLogger.Info("Configmap is not found ", "Configmap.Namespace", instance.Namespace, "ConfigMap.Name", "ibmcloud-cluster-info")
 			reqLogger.Info("Going to create new ConfigMap", "ConfigMap.Namespace", instance.Namespace, "ConfigMap.Name", "ibmcloud-cluster-info")
 			newConfigMap = r.ibmcloudClusterInfoConfigMap(r.client, instance, r.RunningOnOpenShiftCluster(), domainName, r.scheme)
 			err = r.client.Create(context.TODO(), newConfigMap)

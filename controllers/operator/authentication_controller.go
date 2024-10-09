151359e7df6982ee87f514193e654cebeea68ae0
Rob Hundley <rwhundle@us.ibm.com>
2024-10-09 15:56:27 -0400

    Skip handlers that require cluster permissions
    * Skips managing OperandRequests because ODLM requires OLM v0 which in
      turn requires cluster permissions
    * Skips requesting EDB share via CommonService CR because it largely
      depends upon ODLM
    * Skips creating ClusterRole and ClusterRoleBinding for OpenShift
      Authentication
    
    Signed-off-by: Rob Hundley <rwhundle@us.ibm.com>

--- controllers/operator/authentication_controller.go
+++ controllers/operator/authentication_controller.go
@@ -624,7 +624,7 @@ func (r *AuthenticationReconciler) addFinalizer(ctx context.Context, finalizerNa
 	defer r.Mutex.Unlock()
 	if !containsString(instance.Finalizers, finalizerName) {
 		instance.Finalizers = append(instance.Finalizers, finalizerName)
-		err = r.Client.Update(ctx, instance)
+		err = r.Update(ctx, instance)
 	}
 	return
 }
@@ -635,7 +635,7 @@ func (r *AuthenticationReconciler) removeFinalizer(ctx context.Context, finalize
 	defer r.Mutex.Unlock()
 	if containsString(instance.Finalizers, finalizerName) {
 		instance.Finalizers = removeString(instance.Finalizers, finalizerName)
-		err = r.Client.Update(ctx, instance)
+		err = r.Update(ctx, instance)
 		if err != nil {
 			return fmt.Errorf("error updating the CR to remove the finalizer: %w", err)
 		}
@@ -816,9 +816,11 @@ func (r *AuthenticationReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		return
 	}
 	// create clusterrole and clusterrolebinding
-	r.createClusterRole(instance)
+	if subResult, err := r.handleClusterRoles(reconcileCtx, req); subreconciler.ShouldHaltOrRequeue(subResult, err) {
+		return subreconciler.Evaluate(subResult, err)
+	}
 
-	if subResult, err := r.handleClusterRoleBinding(reconcileCtx, req); subreconciler.ShouldHaltOrRequeue(subResult, err) {
+	if subResult, err := r.handleClusterRoleBindings(reconcileCtx, req); subreconciler.ShouldHaltOrRequeue(subResult, err) {
 		return subreconciler.Evaluate(subResult, err)
 	}
 

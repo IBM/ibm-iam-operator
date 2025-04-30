9baf1f69fe8ed14cce06428860061ab3aab5092b
Ujjwal Chakraborty <ujjwal.chakraborty@ibm.com>
2025-04-30 10:41:27 +0530

    version bump for 4.13 CD
    Signed-off-by: Ujjwal Chakraborty <ujjwal.chakraborty@ibm.com>

--- database/schema/v1/tables_test.go
+++ database/schema/v1/tables_test.go
@@ -517,13 +517,13 @@ var _ = DescribeTable("RowDataToAnyMap", func(r RowData, expected map[string]any
 		&Changelog{
 			ID:          5,
 			Name:        "TestChange",
-			IMVersion:   "4.11.0",
+			IMVersion:   "4.12.0",
 			InstallTime: &pgtype.Timestamptz{},
 		},
 		map[string]any{
 			"id":           5,
 			"name":         "TestChange",
-			"im_version":   "4.11.0",
+			"im_version":   "4.12.0",
 			"install_time": &pgtype.Timestamptz{},
 		}),
 	Entry("on OauthToken",

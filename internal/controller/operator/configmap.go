07ea5e8d1b9071cb9e9ea294370d8de9694b2fbc
Rob Hundley <rwhundle@us.ibm.com>
2025-06-30 10:54:29 -0400

    Remove unused ZenExtension code
    Signed-off-by: Rob Hundley <rwhundle@us.ibm.com>

--- internal/controller/operator/configmap.go
+++ internal/controller/operator/configmap.go
@@ -49,6 +49,8 @@ import (
 )
 
 const AnnotationSHA1Sum string = "authentication.operator.ibm.com/sha1sum"
+const ZenProductConfigmapName = "product-configmap"
+const URL_PREFIX = "URL_PREFIX"
 
 // handleConfigMaps is a subreconciler.FnWithRequest that handles the
 // reconciliation of all ConfigMaps created for a given Authentication.

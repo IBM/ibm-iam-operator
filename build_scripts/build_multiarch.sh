616b7a4e8c9e63926ce8bacf42206e61d01e8a0d
Tamilanban Rajendran <tamilanban.rajendran@ibm.com>
2025-07-11 14:59:17 +0530

    Refactored SPS multi arch build and inclused go installation logic

--- build_scripts/build_multiarch.sh
+++ build_scripts/build_multiarch.sh
@@ -2,5 +2,14 @@
 set -e
 
 export REGISTRY="$(get_env DOCKER_REGISTRY || echo docker-na-public.artifactory.swg-devops.com/hyc-cloud-private-scratch-docker-local/ibmcom)"
+GO_VERSION=1.24.1
 
+echo "Installing Go $GO_VERSION"
+curl -sSL "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+echo "Go installed: $(go version)"
 make images

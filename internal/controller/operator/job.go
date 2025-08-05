d52597b852663b839f660f645a4a5a18e7b90b01
Rob Hundley <rwhundle@us.ibm.com>
2025-08-05 09:36:30 -0400

    Pull always
    Signed-off-by: Rob Hundley <rwhundle@us.ibm.com>

--- internal/controller/operator/job.go
+++ internal/controller/operator/job.go
@@ -542,7 +542,7 @@ func buildMigratorContainer(s common.SecondaryReconciler, image string, resource
 	container := corev1.Container{
 		Name:            s.GetName(),
 		Image:           image,
-		ImagePullPolicy: corev1.PullIfNotPresent,
+		ImagePullPolicy: corev1.PullAlways,
 		SecurityContext: &corev1.SecurityContext{
 			SeccompProfile: &corev1.SeccompProfile{
 				Type: corev1.SeccompProfileTypeRuntimeDefault,

3b60703ee1ed5f417d0f8af2eb69c07a11e55a68
Rashmi Khanna <k.rashmi.r@gmail.com>
2025-09-09 19:44:00 +0530

    Add flag for OAuth 2.1 Enablement (#1091)
    Originating issue: [IBMPrivateCloud/roadmap#67163](https://github.ibm.com/IBMPrivateCloud/roadmap/issues/67163)
    
    Signed-off-by: rashmi_kh <rashmi.khanna@in.ibm.com>

--- internal/controller/operator/configmap.go
+++ internal/controller/operator/configmap.go
@@ -310,6 +310,7 @@ func updatePlatformAuthIDP(_ common.SecondaryReconciler, _ context.Context, obse
 			"ATTR_MAPPING_FROM_CONFIG",
 			"AUDIT_URL",
 			"AUDIT_SECRET",
+			"OAUTH_21_ENABLED",
 			"IAM_UM",
 		),
 		updatesValuesWhen(observedKeyValueSetTo[*corev1.ConfigMap]("OS_TOKEN_LENGTH", "45"),
@@ -465,6 +466,11 @@ func (r *AuthenticationReconciler) generateAuthIdpConfigMap(clusterInfo *corev1.
 			return
 		}
 
+		var oauth21Enabled bool
+		if authCR.Spec.Config.OAuth21Enabled != nil {
+			reqLogger.Info("Found OAuth 21 enablement", "OAuth 2.1 enabled", *authCR.Spec.Config.OAuth21Enabled)
+			oauth21Enabled = *authCR.Spec.Config.OAuth21Enabled
+		}
 		var iamUm bool
 		if authCR.Spec.Config.IamUm != nil {
 			reqLogger.Info("Found user management install", "IamUm", *authCR.Spec.Config.IamUm)
@@ -562,6 +568,7 @@ func (r *AuthenticationReconciler) generateAuthIdpConfigMap(clusterInfo *corev1.
 				"SCIM_AUTH_CACHE_TTL_VALUE":          "60",
 				"SCIM_LDAP_ATTRIBUTES_MAPPING":       scimLdapAttributesMapping,
 				"IS_OPENSHIFT_ENV":                   strconv.FormatBool(isOSEnv),
+				"OAUTH_21_ENABLED":                   strconv.FormatBool(oauth21Enabled),
 			},
 		}
 

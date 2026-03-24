ba9f9a5de3d0ba8c6a4816bce6c6e94602896123
Yang Shyr Mei <Shyr.Mei.Yang1@ibm.com>
2026-03-24 19:12:06 +0530

    Revert "Make 'self' as default in auth CR"
    This reverts commit 9e759dbed8490b07e4298a5d906b5c484ee488a3.

--- api/operator/v1alpha1/authentication_types.go
+++ api/operator/v1alpha1/authentication_types.go
@@ -127,8 +127,8 @@ type IngressConfig struct {
 }
 
 type CSPExtensionConfig struct {
-	FrameAncestors []string `json:"frameAncestors"`
-	ConnectSrc     []string `json:"connectSrc"`
+	FrameAncestors []string `json:"frameAncestors,omitempty"`
+	ConnectSrc     []string `json:"connectSrc,omitempty"`
 }
 
 type ConfigSpec struct {
@@ -171,7 +171,7 @@ type ConfigSpec struct {
 	AttrMappingFromConfig       bool                `json:"attrMappingFromConfig,omitempty"`
 	ZenFrontDoor                bool                `json:"zenFrontDoor,omitempty"`
 	Ingress                     *IngressConfig      `json:"ingress,omitempty"`
-	CSPExtension                *CSPExtensionConfig `json:"cspExtension"`
+	CSPExtension                *CSPExtensionConfig `json:"cspExtension,omitempty"`
 	OAuth21Enabled              *bool               `json:"oauth21Enabled,omitempty"`
 	IamUm                       *bool               `json:"iamUm,omitempty"`
 	AccountIamURL               *string             `json:"accountIamURL,omitempty"`
